a5818a1b493bcf10cedcc531f4259772d645733c
Pijush2003 <pijushkantiadak443@gmail.com>
2023-02-22 02:24:40 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/home_page.dart';
+import 'package:flutter_demo/pages/home_page.dart';
 
 void main(List<String> args) {
   runApp(const DemoApp());
