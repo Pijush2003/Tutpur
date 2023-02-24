e65cf8f8cc4608ef5dd7b6bf3c261334ed957f3c
Pijush2003 <pijushkantiadak443@gmail.com>
2023-02-25 01:47:08 +0530

    dat10 completed

---
+++ lib/models/tutpur.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class item {
+  final String id;
+  final String name;
+  final String Description;
+  final String color;
+  final num price;
+  final String image;
+
+  item(
+      {required this.id,
+      required this.name,
+      required this.Description,
+      required this.color,
+      required this.price,
+      required this.image});
+}
+
+final products = [
+  item(
+      id: "Tutpur001",
+      name: "Iphone 12 pro max",
+      Description: "Ai 15 bionic chipset",
+      color: "#33505a",
+      price: 999,
+      image:
+          "https://images.unsplash.com/photo-1617997455403-41f333d44d5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTIlMjBwcm8lMjBtYXh8ZW58MHx8MHx8&w=1000&q=80")
+];
